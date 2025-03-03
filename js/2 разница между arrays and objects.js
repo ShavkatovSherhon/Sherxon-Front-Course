f849e953a0f613fa3895d684b01c6c03d90f98ab
developer sherhon <shavkatovsherhon9@gmail.com>
2025-03-03 11:28:23 +0500

    first commit

---
+++ js/2 разница между arrays and objects.js
@@ -0,0 +1,32 @@
+const arr  = [1, 2, 3]
+const arr3  = ['a', 'b', 'c']
+
+console.log(arr[1]);
+
+const arrObj = {
+    0: 1,
+    1: 2,
+    2: 3,
+}
+
+const arrObj2 = {
+    0: 'a',
+    1: 'b',
+    2: 'c',
+}
+
+const obj = {a: 1, b: 2};
+
+
+
+const obj2  = {
+    'Anna': 500,
+    'Alice': 1000,
+}
+
+
+const arr2  = [
+    'Anna', 500,
+    'Alice', 1000,
+]
+
